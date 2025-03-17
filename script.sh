9da74f0416531b66415670a537b7aaf948fd322a
Jean <jeanrkt08@gmail.com>
2025-03-17 14:29:05 +0300

    Ajout script kill proc +70% (mem / cpu)

--- script.sh
+++ script.sh
@@ -4,6 +4,7 @@ while true; do
 	echo "[1]. Affichage ligne specifique d'un fichier"
 	echo "[2]. Coordonnees d'une ville "
 	echo "[3]. Cherche mot dans un fichier"
+	echo "[4]. Kill un proc +70% (mem / cpu)"
 	echo "[exit]. quitter"
 	read choix
 
@@ -40,6 +41,18 @@ while true; do
 				grep -i "$mot" $fic
 			fi
 		else echo "$fic n'est pas un fichier"
+		fi
+	elif [[ "$choix" == "4" ]]; then
+		proc=$(top -b -n 1 | awk 'NR>7' | head -n 1)
+		cpu=$(echo "$proc" | awk '{print $9}')
+		mem=$(echo "$proc" | awk '{print $10}')
+
+		if [[ $cpu -gt 70 || $mem -gt 70 ]]; then
+			pid=$(echo "$proc" | awk '{print $1}')
+			p_name=$(echo "$proc" | awk '{print $12}')
+			sudo kill -9 $pid
+			echo "killing $p_name..."
+		else	echo "Aucun proc qui consomme +70%( mem / cpu )"
 		fi
 	fi
 done
