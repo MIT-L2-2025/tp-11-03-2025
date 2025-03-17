508087933dcf8bbfa3fcb61be4ec14e9f84a9545
Miah-rktm <rakotomiah8@gmail.com>
2025-03-17 23:35:08 +0300

    Delete exo2_quest3et6.sh
    Fault name of files

--- exo2_quest3et6.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Nombre de villes dans le fichier OSMTowns.txt: "
-c=$(wc -l OSMTowns.txt)
-echo "Il y a: $c villes"
-echo "Ville chien-"
-grep -i "chien" OSMTowns.txt|cut -f1
-echo "Ville chat-"
-grep -i "chat" OSMTowns.txt |cut -f1
