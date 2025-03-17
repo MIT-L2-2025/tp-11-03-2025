01bc17f53a241b96a03ead6f4e03f4b666c05cff
RAZAFITRIMO Tsifeherana Miranto <167677282+Tsifeherana@users.noreply.github.com>
2025-03-17 18:50:21 +0300

    Update exo2.sh

--- exo2.sh
+++ exo2.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#exo2-1)
+wget "http://perso.ens-lyon.fr/etienne.moutot/static/teach/2018-2019/asr2/OSMTowns.txt"
+
+#exo2-2)
+wc -l OSMTowns.txt
+
+#exo2-3)
 echo "La ville la plus a l'Ouest de France : "
 LANG=EN sort OSMTowns.txt --field-separator=$'\t' --general-numeric-sort --key=2|head -n 1
 echo "La ville la plus a l'Est de France  : "
