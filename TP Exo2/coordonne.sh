e8a146238108633b68980f7e7db7fbc86688309c
mihasintsoa <167572977+mihasintsoa@users.noreply.github.com>
2025-03-17 20:06:55 +0300

    coordonne.sh

--- TP Exo2/coordonne.sh
+++ TP Exo2/coordonne.sh
@@ -12,7 +12,7 @@ fi
 
 LATLONG=$(grep "^$T" OSMTowns.txt | head -1)
 
-LAT=$(echo "$LATLONG" | cut -d$'\t' -f 3)
-LONG=$(echo "$LATLONG" | cut -d$'\t' -f 2)
+LAT=$(echo "$LATLONG" | cut -d$'\t' -f 2)
+LONG=$(echo "$LATLONG" | cut -d$'\t' -f 3)
 
 echo "la ville $T a pour latitude $LAT et longitude $LONG"
