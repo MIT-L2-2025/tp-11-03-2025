8d4b98bc9a25def91d26581df561ec0e0a585718
Nancia RAHARINIRINA <167851356+Rhren@users.noreply.github.com>
2025-03-17 21:26:18 +0300

    Update Exo2_n6.sh

--- Exo2_n6.sh
+++ Exo2_n6.sh
@@ -4,4 +4,4 @@ echo "Affichage des villes contenant chien dans leur nom"
 grep "chien" OSMTowns.txt 
 
 echo "Affichage des villes contenant chat/Chat dans leur nom"
-(ou grep -i "chat" OSMTowns.txt)
+grep -i "chat" OSMTowns.txt)
