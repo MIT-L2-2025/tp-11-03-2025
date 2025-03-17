b9d31033e8dbae5f1376328a93f85ad4b101c80f
RAZAFITRIMO Tsifeherana Miranto <167677282+Tsifeherana@users.noreply.github.com>
2025-03-17 18:46:27 +0300

    Update coordonnees.sh

--- coordonnees.sh
+++ coordonnees.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#exo2-4)
 latlong=$(grep ^$1 OSMTowns.txt | head -n1)
 echo "$latlong"
 name_ville=$(echo "$latlong" | cut -d$'\t' --field=1)
@@ -8,8 +9,10 @@ long=$(echo "$latlong" | cut -d$'\t' --field=2)
 
 echo "Coordonnées de $name_ville : latitude : $lat , longitude : $long"
 
+#exo2-5)
 echo "Lien de coordonnees : https://www.openstreetmap.org/#map=18/$lat/$long"
 
+#exo2-6)
 grep "chien" OSMTowns.txt
 grep -i "chat" OSMTowns.txt
 
