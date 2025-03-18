acdbbfba5b7eb9c9611a1874666967da436d2e13
IavoAmbinintsoa <167605546+IavoAmbinintsoa@users.noreply.github.com>
2025-03-18 00:44:04 -0500

    Update and rename exo2_5.sh to liens_coordone.sh

--- TP1/liens_coordone.sh
+++ TP1/liens_coordone.sh
@@ -3,7 +3,3 @@ LATLONG=$(grep "^$1	" OSMTowns.txt|head -n1)
 LAT=$(echo "$LATLONG" |cut --delimiter "	" --fields=3)
 LONG=$(echo "$LATLONG" |cut --delimiter "	" --fields=2)
 echo "https://ww.openstreetmap.org/#map=18/$LAT/$LONG"
-echo " "
-echo " "
-grep "chien" OSMTowns.txt
-grep -i "chat" OSMTowns.txt
