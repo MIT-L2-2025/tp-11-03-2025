3c82dd4c5dc9756d611e9cc861df784ab87090b0
IavoAmbinintsoa <167605546+IavoAmbinintsoa@users.noreply.github.com>
2025-03-18 00:45:15 -0500

    chat_Chat_chien

--- TP1/exo2_6.sh
+++ TP1/exo2_6.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+echo "La ville qui a une nom avec chat et Chat : "
+echo ""
+grep -i "chat" OSMTowns.txt
 echo "La ville qui a une nom avec chiens : "
 echo ""
 grep "chien" OSMTowns.txt
-echo "La ville qui a une nom avec chiens : "
-echo ""
-grep -i "chat" OSMTowns.txt
