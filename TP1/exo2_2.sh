159a9f5879f93856454160b25193c93189fc0920
IavoAmbinintsoa <167605546+IavoAmbinintsoa@users.noreply.github.com>
2025-03-18 00:41:07 -0500

    nbr_ville

--- TP1/exo2_2.sh
+++ TP1/exo2_2.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-echo "Telechargement du fichier OSMTowns.txt..."
-wget -q https://emoutot.perso.math.cnrs.fr/static/teach/2018-2019/asr2/OSMTowns.txt -O OSMTowns.txt
-
-# Vérification du téléchargement
+# Vérification
 if [ ! -f OSMTowns.txt ]; then
-    echo "Erreur : impossible de télécharger OSMTowns.txt."
+    echo "Erreur : le fichier OSMTowns.txt est introuvable ."
     exit 1
 fi
 
-echo ""
 echo "Nombre de villes : $(wc -l < OSMTowns.txt)"
