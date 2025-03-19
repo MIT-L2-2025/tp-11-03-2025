903bc58ee4d94b3f0017299c5b169da0178e6aaf
ANDRIAMANDRATOMANANA  Narindrantsoa <167776916+ANDRIAMANDRATOMANANA@users.noreply.github.com>
2025-03-19 08:10:59 +0300

    Delete exo2_4_5.sh

--- exo2_4_5.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "4)Retour coordoonées d'une ville:"
-lat_long=$(grep "$1" "OSMTowns.txt"|head -n1)
-long=$(echo "$lat_long" |cut  -f2)
-lat=$(echo "$lat_long" |cut  -f3)
-
-echo "La ville $1 a pour longitude $long et latitude $lat"
-echo "lien:"
-echo "https://www.openstreetmap.org/#map=18/$lat/$long"
