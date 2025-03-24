2727e04be3ca74a6f2159d6809d530f2002a0981
ANDRIAMANDRATOMANANA  Narindrantsoa <167776916+ANDRIAMANDRATOMANANA@users.noreply.github.com>
2025-03-24 19:19:29 +0300

    Add files via upload

---
+++ all_exo3.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+echo "1-calcul distance par rapport a Lyon "
+echo "2-affichage lien"
+read -p "entrez:" n
+case $n in
+    1)
+        echo "-----------------------------------------------"
+        read -p "entrez le nom du ville " ville
+        lat_long_lyon=$(grep "Lyon" "OSMTowns.txt"|head -n1)
+        long_lyon=$(echo "$lat_long_lyon" |cut  -f2)
+        lat_lyon=$(echo "$lat_long_lyon" |cut  -f3)
+
+        lat_long=$(grep "$ville" "OSMTowns.txt"|head -n1)
+        long=$(echo "$lat_long" |cut  -f2)
+        lat=$(echo "$lat_long" |cut  -f3)
+        python3 gps-dist.py "$lat_lyon" "$long_lyon" "$lat" "$long"
+    ;;
+    2)
+        echo "-----------------------------------------------"
+        read -p "entrez le nom du ville " ville
+        API_URL="https://fr.wikipedia.org/w/api.php"
+
+        PARAMS="action=query&titles=${ville}&prop=pageimages&pithumbsize=600&format=json&formatversion=2"
+
+        PARAMS=$(echo -n "$PARAMS" | sed 's/ /%20/g')
+
+        response=$(curl -s "${API_URL}?${PARAMS}")
+
+        if echo "$response" | grep -q "missing"; then
+            echo "Erreur : La page pour '$ville' n'existe pas sur Wikipédia en français."
+            exit 1
+        fi
+
+        image_url=$(echo "$response" | jq -r '.query.pages[0].thumbnail.source // "Aucune image trouvée"')
+
+        if [ "$image_url" = "Aucune image trouvée" ]; then
+            echo "Aucune image associée à '$ville' n'a été trouvée."
+        else
+            echo "Lien de la première image pour '$ville' : $image_url"
+        fi
+
+    ;;
+    *)
+        echo "choix invalide"
+    ;;
+esac
+
+
+
