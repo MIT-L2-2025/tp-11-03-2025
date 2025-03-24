f16ab4e98f4747c0e14f33af5b96bc33ffaff34d
mihasintsoa <167572977+mihasintsoa@users.noreply.github.com>
2025-03-24 19:16:28 +0100

    main.sh

--- TP Exo2/main.sh
+++ TP Exo2/main.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+if [ $# -lt 1  ]; then
+  exit
+fi
 
 bash ville.sh
 bash coordonne.sh $1
 bash openStreetMap.sh $1
-echo "Ensemble de vill e contenant le mot chat/Chat et chien:"
+echo "Ensemble de ville contenant le mot chat/Chat et chien:"
 bash search.sh
 
